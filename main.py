28243604f5241172a19d7e3ad4f923558e4d2714
bursal0 <154094988+bursal0@users.noreply.github.com>
2026-04-19 20:39:53 +0300

    .
    .

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import joblib
 import pandas as pd
 import numpy as np
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 app = FastAPI()
 
+# Model yükleme
 try:
     model = joblib.load("final_model.pkl")
     print("Model başarıyla yüklendi 🚀")
 except Exception as e:
     print("Model yüklenemedi:", e)
 
+# Templates
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"message": "API çalışıyor"}
+# 🔥 SADECE BU KALSIN
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
+# Predict endpoint
 @app.post("/predict")
 def predict(data: dict):
     try:
         df = pd.DataFrame([data])
-
         prediction = model.predict(df)
         price = np.expm1(prediction[0])
-
         return {"predicted_price": float(price)}
-
     except Exception as e:
         return {"error": str(e)}
-
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request}
